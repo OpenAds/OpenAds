065d930dbb6bdbebe15277ef5d36707659717d69
Novacha Man <novachalon@gmail.com>
2013-08-28 09:48:30 +1000

    Added south migrations

--- openads/settings.py
+++ openads/settings.py
@@ -129,6 +129,9 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+
+    # Packages
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
