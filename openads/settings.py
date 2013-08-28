f9dde0bbce04e772a31bd068137e634eb0a77ac9
Novacha Man <novachalon@gmail.com>
2013-08-28 15:21:04 +1000

    Moved to mysql. This will be a requirement

--- openads/settings.py
+++ openads/settings.py
@@ -14,12 +14,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(BASE_PATH, 'dev.db'),  # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
+        'ENGINE': 'django.db.backends.mysql',  # Only mysql support
+        'NAME': 'adverts',
+        'USER': 'root',
         'PASSWORD': '',
-        'HOST': '',
+        'HOST': 'localhost',
         'PORT': '',
     }
 }
