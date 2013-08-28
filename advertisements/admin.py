43b1588d036f8a0a101fcfd2adc2a57e37bf15e3
Novacha Man <novachalon@gmail.com>
2013-08-28 14:23:46 +1000

    Better admin for adverts

--- advertisements/admin.py
+++ advertisements/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from advertisements.models import Advertisement, Provider
 
 
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ('provider', 'ad_type', 'image', 'created')
+    date_hierarchy = 'created'
+    list_filter = ('ad_type', 'created')
+
+
 admin.site.register(Provider)
-admin.site.register(Advertisement)
+admin.site.register(Advertisement, AdvertisementAdmin)
