6b6da58a2dafd1efb2c3385f92ca481febe51b58
Novacha Man <novachalon@gmail.com>
2013-08-28 10:08:53 +1000

    Added media serving in development

--- openads/urls.py
+++ openads/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
+from django.conf import settings
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -13,3 +15,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
