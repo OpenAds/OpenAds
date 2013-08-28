13ee522815603e66e595fba6a390b83be0c84ded
Novacha Man <novachalon@gmail.com>
2013-08-28 14:59:42 +1000

    Added filtering for enabled ads

--- advertisements/views.py
+++ advertisements/views.py
@@ -12,9 +12,9 @@ def click_register(request, ad_pk):
 
 
 def top_ad(request):
-    if not Advertisement.objects.filter(ad_type=Advertisement.TOP_AD).exists():
+    if not Advertisement.objects.filter(ad_type=Advertisement.TOP_AD, enabled=True).exists():
         return HttpResponse("No adverts") # TODO: Placeholder
-    advert = Advertisement.objects.filter(ad_type=Advertisement.TOP_AD).order_by('?')[0]
+    advert = Advertisement.objects.filter(ad_type=Advertisement.TOP_AD, enabled=True).order_by('?')[0]
 
     return render(request, 'advertisements/top_ad.html', {
         "advert": advert,
@@ -22,9 +22,9 @@ def top_ad(request):
 
 
 def side_ads(request):
-    if not Advertisement.objects.filter(ad_type=Advertisement.SIDE_AD).exists():
+    if not Advertisement.objects.filter(ad_type=Advertisement.SIDE_AD, enabled=True).exists():
         return HttpResponse("No adverts")  # TODO: Placeholder
-    adverts = Advertisement.objects.filter(ad_type=Advertisement.SIDE_AD).order_by('?')[:4]
+    adverts = Advertisement.objects.filter(ad_type=Advertisement.SIDE_AD, enabled=True).order_by('?')[:4]
 
     return render(request, 'advertisements/side_ads.html', {
         "adverts": adverts,
