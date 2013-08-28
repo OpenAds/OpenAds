5958756aee55bb0d7110a67d314d05894606e2ec
Novacha Man <novachalon@gmail.com>
2013-08-28 11:37:22 +1000

    Added base for top adverts

--- advertisements/views.py
+++ advertisements/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, render
 from advertisements.models import Advertisement
 
 
@@ -9,3 +9,13 @@ def click_register(request, ad_pk):
     advert.clicked()
 
     return HttpResponseRedirect(advert.url)
+
+
+def top_ad(request):
+    if not Advertisement.objects.exists():
+        return HttpResponse("No adverts") # TODO: Placeholder
+    adverts = Advertisement.objects.order_by('?')[:4]
+
+    return render(request, 'advertisements/top_ad.html', {
+        "adverts": adverts,
+    })
