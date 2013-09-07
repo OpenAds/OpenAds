fd37ad16ba03dd3ecea1018294e8190014e96c0e
Novacha Man <novachalon@gmail.com>
2013-09-07 17:33:19 +1000

    Added link to advert stats

--- advertisements/urls.py
+++ advertisements/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('advertisements.views',
     url(r'^sides/$', 'side_ads'),
 
     url(r'^provider/(?P<provider_pk>\d+)/$', 'view_provider_statistics'),
-    url(r'^advertisement/(?P<advert_pk>\d+)/$', 'view_advert_statistics')
+    url(r'^advertisement/(?P<advert_pk>\d+)/$', 'view_advert_statistics', name="advert_statistics")
 )
