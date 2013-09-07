db56e011291c225fc76543c6b3dc941647ffc9af
Novacha Man <novachalon@gmail.com>
2013-09-07 23:57:14 +1000

    Test uses sqlite

--- openads/settings.py
+++ openads/settings.py
@@ -23,6 +23,9 @@ DATABASES = {
     }
 }
 
+if os.getenv('TRAVIS', False):
+    DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3'}}
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
