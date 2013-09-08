6e743be231379d7a22478cb05a81712c85651913
Novacha Man <novachalon@gmail.com>
2013-09-08 17:25:24 +1000

    Moved to status in views

--- advertisements/views.py
+++ advertisements/views.py
@@ -15,9 +15,9 @@ def click_register(request, ad_pk):
 
 
 def top_ad(request):
-    if not Advertisement.objects.filter(ad_type=Advertisement.TOP_AD, enabled=True).exists():
+    if not Advertisement.objects.filter(ad_type=Advertisement.TOP_AD, status=Advertisement.ACTIVE).exists():
         return HttpResponse("No adverts") # TODO: Placeholder
-    advert = Advertisement.objects.filter(ad_type=Advertisement.TOP_AD, enabled=True).order_by('?')[0]
+    advert = Advertisement.objects.filter(ad_type=Advertisement.TOP_AD, status=Advertisement.ACTIVE).order_by('?')[0]
 
     return render(request, 'advertisements/top_ad.html', {
         "advert": advert,
@@ -25,9 +25,9 @@ def top_ad(request):
 
 
 def side_ads(request):
-    if not Advertisement.objects.filter(ad_type=Advertisement.SIDE_AD, enabled=True).exists():
+    if not Advertisement.objects.filter(ad_type=Advertisement.SIDE_AD, status=Advertisement.ACTIVE).exists():
         return HttpResponse("No adverts")  # TODO: Placeholder
-    adverts = Advertisement.objects.filter(ad_type=Advertisement.SIDE_AD, enabled=True).order_by('?')[:4]
+    adverts = Advertisement.objects.filter(ad_type=Advertisement.SIDE_AD, status=Advertisement.ACTIVE).order_by('?')[:4]
 
     return render(request, 'advertisements/side_ads.html', {
         "adverts": adverts,
@@ -66,8 +66,8 @@ def view_provider_statistics(request, provider_pk):
 
     return render(request, 'advertisements/statistics/provider_statistics.html', {
         "provider": provider,
-        "active_ads": provider.advertisement_set.filter(enabled=True),
-        "inactive_ads": provider.advertisement_set.filter(enabled=False),
+        "active_ads": provider.advertisement_set.filter(status=Advertisement.ACTIVE),
+        "inactive_ads": provider.advertisement_set.filter(status=Advertisement.INACTIVE),
     })
 
 
