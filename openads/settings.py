8aafde303066cd9a3613d0ddeaff3e09a2f66a1e
Novacha Man <novachalon@gmail.com>
2013-09-08 16:13:21 +1000

    Removed sites framework

--- openads/settings.py
+++ openads/settings.py
@@ -40,8 +40,6 @@ TIME_ZONE = 'America/Chicago'
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
-SITE_ID = 1
-
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
@@ -125,7 +123,6 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
