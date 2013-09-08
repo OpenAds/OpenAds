d66940ca82e9ae7f54c207a519cd42b8f03a6e8b
Novacha Man <novachalon@gmail.com>
2013-09-08 11:29:53 +1000

    Added a password change from using crispy-forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordChangeForm
 from django.shortcuts import render
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from django.contrib import messages
 
 
 @login_required
 def change_password(request):
+
+    helper = FormHelper()
+    helper.add_input(Submit('submit', 'Submit'))
 
     if request.method == "POST":
         form = PasswordChangeForm(
@@ -18,6 +24,7 @@ def change_password(request):
     else:
         form = PasswordChangeForm(user=request.user)
 
+    form.helper = helper
     return render(request, 'accounts/password_change.html', {
         "form": form
     })
