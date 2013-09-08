92d14f8c4e48bb08fa4871ace48660109002989f
Novacha Man <novachalon@gmail.com>
2013-09-08 17:30:48 +1000

    Added tests to cover count methods

--- advertisements/tests.py
+++ advertisements/tests.py
@@ -314,7 +314,6 @@ class ClickRegisterTest(TestCase):
         self.provider_adverts = mommy.make(Advertisement, _quantity=10, provider=self.provider)
 
     def tearDown(self):
-        self.client.logout()
         self.provider.delete()
         self.user.delete()
 
@@ -341,3 +340,49 @@ class ClickRegisterTest(TestCase):
                     follow=True
                 )
                 self.assertEqual(advert.click_set.count(), i+1)
+
+
+class ProviderCountMethodTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('admin', 'test@example.com', 'pass')
+        self.user.is_superuser = True
+        self.user.is_staff = True
+        self.user.save()
+
+        self.provider = Provider(
+            name='provider',
+            user=self.user,
+        )
+        self.provider.save()
+
+        self.provider_active_adverts = mommy.make(
+            Advertisement, _quantity=20,
+            provider=self.provider,
+            status=Advertisement.ACTIVE
+        )
+        self.provider_inactive_adverts = mommy.make(
+            Advertisement, _quantity=20,
+            provider=self.provider,
+            status=Advertisement.INACTIVE
+        )
+        self.provider_pending_adverts = mommy.make(
+            Advertisement, _quantity=20,
+            provider=self.provider,
+            status=Advertisement.PENDING
+        )
+
+    def tearDown(self):
+        self.provider.delete()
+        self.user.delete()
+
+    def test_active_ads_returns_correct_amounts(self):
+        """
+        Test that the active_ads method on a provider returns the correct amount
+        """
+        self.assertEqual(self.provider.active_ads(), 20)
+
+    def test_inactive_ads_returns_correct_amounts(self):
+        """
+        Test that the inactive_ads method on a provider returns the correct amount
+        """
+        self.assertEqual(self.provider.inactive_ads(), 20)
