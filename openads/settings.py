f60b96991666e464a4479976048e50ab77b3e1f1
Novacha Man <novachalon@gmail.com>
2013-09-16 19:02:42 +1000

    Added HTML compression

--- openads/settings.py
+++ openads/settings.py
@@ -104,6 +104,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'htmlmin.middleware.HtmlMinifyMiddleware',
 )
 
 ROOT_URLCONF = 'openads.urls'
